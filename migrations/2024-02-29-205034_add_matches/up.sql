78d654b4ebb01a6afeb0f6f00cab021ff82dd73e
Gustav Thoft <35370673+TotallyNotRust@users.noreply.github.com>
2024-02-29 22:15:16 +0100

    working on migrations

--- migrations/2024-02-29-205034_add_matches/up.sql
+++ migrations/2024-02-29-205034_add_matches/up.sql
@@ -1,5 +1,5 @@
 -- Your SQL goes here
-CREATE TABLE IF NOT EXISTS matches (
+CREATE TABLE IF NOT EXISTS made_matches ( /* match and matches are both keywords so i improvise... */
     id bigint not null AUTO_INCREMENT,
     gamemode int not null,
     PRIMARY KEY ( id )
